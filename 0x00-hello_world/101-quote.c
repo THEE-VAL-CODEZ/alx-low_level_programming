848acd00a2590a856dbd81da0e4cc89b1f4b81c0
Valerie Omondi <valerieomondi5@gmail.com>
2022-06-30 15:23:27 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
- 
+
 /**
  *  main - Entry point
  *
@@ -10,16 +10,17 @@
 
 int main(void)
 {
-	char *s="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	long l = 59;
 	long fd = 1;
 	long syscall = 1;
 	long ret = 0;
+
 	__asm__("syscall"
-	:"=a" (ret)
+	: "=a" (ret)
 	: "a" (syscall),
 	"D" (fd),
 	"S" (s),
-        "d" (l));	
-	return (1);	
+	"d" (l);
+	return (1);
 }
