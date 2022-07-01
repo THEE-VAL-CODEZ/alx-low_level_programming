e26263c1d2819a9939e77dca04c381ed7ffe4b65
Valerie Omondi <valerieomondi5@gmail.com>
2022-07-01 05:04:59 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: 0 (success)
+ */
+
+int main(void)
+{
+	char ch = a;
+
+	for (; ch <= z; ch++)
+	{
+		putchar(ch'\n');
+	}
+	return (0);
+}
