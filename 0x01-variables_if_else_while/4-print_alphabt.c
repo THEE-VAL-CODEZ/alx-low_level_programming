9e1ad18b29b7aafdaa6e427d3947beedd5126533
Valerie Omondi <valerieomondi5@gmail.com>
2022-07-01 06:49:16 -0700

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0(success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	while (ch != 113; ch != 101)
+	{
+		for (ch = 97; ch <= 122; ch++)
+			putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
+}
