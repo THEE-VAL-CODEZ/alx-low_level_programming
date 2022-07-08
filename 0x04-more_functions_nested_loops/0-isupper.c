e5b457ad6752bbc122dbea8c996bb7a2a7b8bfab
Valerie Omondi <valerieomondi5@gmail.com>
2022-07-08 07:24:19 -0700

    type alphabet

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ *  main - check the code
+ *
+ *  Return: Always 0.
+ */
+
+
+int _isupper(int c);
+
+
+int main(void)
+
+{
+	char c;
+
+	if (ch >= 'A' && ch <= 'Z')
+		printf("%c: %d\n", ch,  _isupper(c));
+			return (1);
+	
+	else
+
+		printf("%c: %d\n", ch,  _isupper(c));
+			return (0);
+}	
+	
+
