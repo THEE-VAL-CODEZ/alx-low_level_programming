1ad1850f0ad7fa0fac8ed46b722d6d6b31778c53
Valerie Omondi <valerieomondi5@gmail.com>
2022-07-12 06:14:14 -0700

    Return legnth of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _strlen - returns the legnth of a string
+ *
+ *  @s: char type pointer
+ *
+ *  Return: legnth
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		return (c);
+	}
+}
