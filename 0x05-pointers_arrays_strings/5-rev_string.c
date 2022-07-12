c4eff2f32751961726554c8ff44bb255cebf6860
Valerie Omondi <valerieomondi5@gmail.com>
2022-07-12 07:49:48 -0700

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  rev_string - Reverses strings
+ *
+ *  @s: char type string
+ */
+
+void rev_string(char *s)
+{
+	int i, c, l;
+
+	char h;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	l = i;
+
+	for (i--, c = 0; c < 1 / 2; i--, c++)
+	{
+		h = s[c];
+
+		s[c] = s[i];
+
+		s[i] = h;
+	}
+}
