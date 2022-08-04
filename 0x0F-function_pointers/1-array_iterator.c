c6d4ec3b9215d3d4f9cf95a592b1fae16aac2d2a
Valerie Omondi <valerieomondi5@gmail.com>
2022-08-04 04:37:58 -0700

    executes function given as a parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  array_iterator - iterates through an array
+ *  @array: passed pointers to an array
+ *  @size: size of  array
+ *  @action: void pointer function
+ */
+
+void array_iterator(int *array, size_t size, void (*action) (int))
+{
+	unsigned int i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+
+		{
+			action(array[i]);
+		}
+	}
+}
