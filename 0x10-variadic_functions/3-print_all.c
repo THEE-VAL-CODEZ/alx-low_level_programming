7492d8d5f10743059779840c5d099bde81e85df1
Valerie Omondi <valerieomondi5@gmail.com>
2022-08-05 01:03:42 -0700

    prints anyting

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,42 +1,99 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+#include <stdio.h>
 
 /**
- * print_all - prints char, string, int, or char
- *
- * @format: what type to print
+ * print_c - print char
+ * @c: char to print
+ * Return: void
+ */
+
+void print_c(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * print_s - prints string
+ * @s: string to print
+ * Return: void
+ */
+
+void print_s(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ *  print_i - prints an int
+ *  @i: int to print
+ *  Return: void
+ */
+
+void print_i(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_f - prints floats
+ * @f: float to print
+ * Return: void
+ */
+
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of arguments types passed to the function
+ * Return: void
  */
 
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	int space;
-	char *string;
+	unsigned int i, j;
+	print_t p[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"s", print_s},
+		{"f", print_f},
+		{NULL, NULL}
+	};
+	va_list valist;
+	char *separator = "";
 
-	va_list ap;
+	va_start valist;
+	char *separator = "";
 
-	va_start(ap, format);
+	va_start(valist, format);
+	i = 0;
 
-		while (format && format[i])
+	while (format && format[i])
+	{
+		j = 0;
+
+		while (p[j].t != NULL)
+
 		{
-			space = 0;
-			switch (format[i])
+			if (*(p[j].t) == format[i])
 			{
-				case 'c':
-					printf("%c", va_arg(ap, int));
-					space = 1;
-					break;
-				case 'i':
-					printf("%d", va_arg(ap, int));
-					space = 1;
-					break;
+				printf("%s", separator);
+				p[j].f(valist);
+				separator = ", ";
+
+				break;
 			}
-			if (space == 1 && format[i + 1])
-				printf(", ");
-			i++;
+			j++;
 		}
-	va_end(ap);
+		i++;
+	}
+	va_end(valist);
 	printf("\n");
 }
